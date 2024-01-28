e6b9c6abd501480b725d70f8c6f5794ba4f67272
Joannamcgallagher <joannamcgallagher@gmail.com>
2024-01-28 13:15:46 +0000

    Add student.py file and initialize

--- evens.py
+++
@@ -1,5 +0,0 @@
-def even_number_of_evens(numbers):
-    return True
-
-if __name__ == "__main__":
-    print(even_number_of_evens(5))
--- test_evens.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from evens import even_number_of_evens
-
-class TestEvens(unittest.TestCase):    
-    def test_function_returns_True(self):
-        self.assertTrue(even_number_of_evens([]))
-
-
-unittest.main()
